cd67301c053c9ef89e45dda20fb1b38c5076b22b
Douglas  Rodrigues <170281719+rodriguesdo@users.noreply.github.com>
2024-08-18 23:51:48 -0300

    Delete Média de dois números - Exercício 03.py

--- Média de dois números - Exercício 03.py
+++
@@ -1,7 +0,0 @@
-nome = input('Digite o nome do aluno(a): ')
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-
-media =(n1+n2)/2
-
-print(f"Olá {nome}, sua media foi: {media}")
