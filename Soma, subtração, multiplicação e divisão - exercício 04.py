f000d7c97aee2afe8d587f7d1b56dc2f703b1d2c
Douglas  Rodrigues <170281719+rodriguesdo@users.noreply.github.com>
2024-08-18 23:52:05 -0300

    Delete Soma, subtração, multiplicação e divisão - exercício 04.py

--- Soma, subtração, multiplicação e divisão - exercício 04.py
+++
@@ -1,7 +0,0 @@
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-soma = (numero1+numero2)
-subtracao = (numero1-numero2)
-multiplicacao = (numero1*numero2)
-divisao = (numero1/numero2)
-print(soma, subtracao, multiplicacao, divisao)
